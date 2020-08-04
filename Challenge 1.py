24702a6392c6d39edfed4726806e2d1d473713c7
Jonathany25 <jyunawan@gmail.com>
2020-08-04 20:13:50 +0700

    Made some changes

--- Challenge 1.py
+++ Challenge 1.py
@@ -1,19 +1,11 @@
-from pynput.keyboard import  Key, Listener
 from colorama import init,Fore, Back, Style
 init()
 
 print("Why does Waldo wear stripes?")
+input("Press ENTER for the punch line!")
+print(Fore.RED + "Because he doesn’t want to be spotted.")
 
-def on_press(key):
-    pass
 
-def on_release(key):
-    if key == Key.enter:
-        print(Fore.RED + "Because he doesn’t want to be spotted.")
-        return False
-
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
 
 
 
--- Challenge 6.py
+++ Challenge 6.py
@@ -1,37 +1,27 @@
-from pynput.keyboard import  Key, Listener
+
 import datetime as dt
 
 state = 0
 first = ""
 last = ""
 time = 10
+redo = "yes"
 
-print("Try to guess how long " + str(time) +" seconds is!\nPress ENTER to start and stop the timer!")
-
-def on_press(key):
-    global state
-    global last
-    global first
-    keys = key
-    if keys == Key.enter:
-        if state == 1:
-            last = dt.datetime.now()
-            delta = last - first
-            time_off = abs(time - float(delta.total_seconds()))
-            print("You guessed " + str(round(delta.total_seconds(),2)) + " seconds. You were " + str(round(time_off,2)) + " seconds off.")
-            return False
-
-def on_release(key):
-    global state
-    global last
-    global first
-    keys = key
-    if keys == Key.enter:
-        if state == 0:
-            first = dt.datetime.now()
-            state = 1
+while redo.lower()=="yes":
+    print("\nTry to guess how long " + str(time) +" seconds is!\nPress ENTER to start and stop the timer!")
+    input("Press ENTER to start!")
+    first = dt.datetime.now()
+    input("Press ENTER to stop!")
+    last = dt.datetime.now()
+    delta = last - first
+    time_off = abs(time - float(delta.total_seconds()))
+    print("You pressed at " + str(round(delta.total_seconds(),2)) + " seconds. You were " + str(round(time_off,2)) + " seconds off.")
 
 
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+
+
+
+
+
+
